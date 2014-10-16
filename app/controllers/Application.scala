ebb09e1464201e7b393bd6c4e2e4aede3a8d2261
Dmitry Baskakov <dmitry.baskakov.github@gmail.com>
2014-10-16 15:04:28 +0400

    Another SMTP checker try

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,7 +4,7 @@ import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
-import com.arcww.smtp.SMTPSession
+import com.arcww.smtp.{SMTPMXLookup, SMTPSession}
 
 case class CheckData(host: String, port: Int, email: String)
 
@@ -26,14 +26,15 @@ object Application extends Controller {
   def submit = Action { implicit request =>
     val data = checkForm.bindFromRequest.value.map({
       case CheckData(host,port,email) =>
-        val session = new SMTPSession(host, port, email, "test@mail.ru", "Subject", "Body")
+        /*val session = new SMTPSession(host, port, email, "test@mail.ru", "Subject", "Body")
         val res = try {
           session.sendMessage()
         }
         catch {
           case e => session.log + "\r\n --------------------------------------- \r\n        " + e.getMessage
-        }
-        res
+        }*/
+        import scala.collection.JavaConversions._
+        SMTPMXLookup.isAddressValid(email).toIndexedSeq.toList
     })
 
     Ok(views.html.index(checkForm.bindFromRequest, data))
